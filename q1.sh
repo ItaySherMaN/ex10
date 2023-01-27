b5f9cb1b0d2dad9c6dfe35fb7ce40492bf8f47f6
Uri Segman <uri.segman@post.idc.ac.il>
2023-01-27 22:45:43 +0200

    I have added my solution to q1, and with it added copies of the unzipped text files for reference

--- q1.sh
+++ q1.sh
@@ -1,1 +1,21 @@
-This is q1 answer
+# /bin/bash
+#a.
+
+awk 'END {print NR}' aliceinwonderland.txt
+
+#b
+
+awk '/Alice/' aliceinwonderland.txt | awk ' END { print NR}'
+
+#c
+
+awk '{for(i=1; i<=NF; i++) if ($i=="Alice") {count++}} END {print "the word Alice shows up ",count," of times"}' aliceinwonderland.txt
+
+#d
+
+awk '{if (length($0) > max) max = length($0)} END {print "the largest row has " max " letters"}' aliceinwonderland.txt
+
+
+#e
+
+grep '\S' aliceinwonderland.txt | awk '{for(i=1; i<=NF; i++) {a[$i]++}} END {for (k in a) print a[k], k}' | sort -rn | head -5
