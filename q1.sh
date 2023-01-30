0c200e61e1493fe5541561df1ece763b559fafd9
Tom Saacks <saackstom2306@gmail.com>
2023-01-30 16:40:49 +0200

    Tom Saacks fisrt commit

--- q1.sh
+++ q1.sh
@@ -1,1 +1,6 @@
-This is q1 answer
+awk 'END{print NR}' aliceinwonderland.txt
+awk '/Alice/ {print NR}' aliceinwonderland.txt| wc -l
+awk '/ Alice /{i++}END{print i}' aliceinwonderland.txt
+awk '{print length, $0}' aliceinwonderland.txt |sort -nr|head -1 | cut -d " " -f 2-
+awk '{print tolower($0)}' aliceinwonderland.txt |awk '{gsub(/[^[:alnum:]]/, " "); for (i=1;i<=NF;i++) {count[$i]++}} END {for (word in count) {print count[word], word}}' | sort -nr | head -5
+
